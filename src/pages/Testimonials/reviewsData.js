4ccfcb9196b7906892c55d7793340bcf6dde3fe0
johnndi <159266362+johnndi@users.noreply.github.com>
2024-06-09 16:37:13 +0300

    Update reviewsData.js

--- src/pages/Testimonials/reviewsData.js
+++ src/pages/Testimonials/reviewsData.js
@@ -1,32 +1,32 @@
-import spenser from "../../pages/Testimonials/spenser.jpg"
+
 export const reviews = [
   {
     id: 1,
-    image: spenser,
+    
     name: "Johnndi",
     text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque corporis blanditiis id odit corrupti eaque culpa facilis harum suscipit qui et error similique iure maxime maiores officia rem quasi, repudiandae labore dolorem cupiditate numquam vitae atque ex. Iure nisi cum mollitia accusamus!.",
   },
   {
     id: 2,
-    image: "./ass",
+    
     name: "Jane ",
     text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque corporis blanditiis id odit corrupti eaque culpa facilis harum suscipit qui et error similique iure maxime maiores officia rem quasi, repudiandae labore dolorem cupiditate numquam vitae atque ex. Iure nisi cum mollitia accusamus",
   },
   {
     id: 3,
-    image: spenser,
+  
     name: "Alex ",
     text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque corporis blanditiis id odit corrupti eaque culpa facilis harum suscipit qui et error similique iure maxime maiores officia rem quasi, repudiandae labore dolorem cupiditate numquam vitae atque ex. Iure nisi cum mollitia accusamus",
   },
   {
     id: 4,
-    image: spenser,
+    
     name: "Emily ",
     text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque corporis blanditiis id odit corrupti eaque culpa facilis harum suscipit qui et error similique iure maxime maiores officia rem quasi, repudiandae labore dolorem cupiditate numquam vitae atque ex. Iure nisi cum mollitia accusamus!",
   },
   {
     id: 5,
-    image: spenser,
+    
     name: "David Miller",
     text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque corporis blanditiis id odit corrupti eaque culpa facilis harum suscipit qui et error similique iure maxime maiores officia rem quasi, repudiandae labore dolorem cupiditate numquam vitae atque ex. Iure nisi cum molliti",
   },
